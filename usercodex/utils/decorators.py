b8231cdac181554d14cac597f33a44b6d630c576
KENZO404 <80487896+KENZO-404@users.noreply.github.com>
2021-07-24 14:08:59 +0000

    pylint: auto fixes

--- usercodex/utils/decorators.py
+++ usercodex/utils/decorators.py
@@ -10,10 +10,10 @@ from ..Config import Config
 from ..core.data import _sudousers_list, blacklist_chats_list
 from ..core.events import MessageEdited, NewMessage
 from ..core.logger import logging
+from ..core.session import codex
 from ..helpers.utils.format import paste_message
 from ..helpers.utils.utils import runcmd
 from ..sql_helper.globals import gvarstatus
-from ..core.session import codex
 
 LOGS = logging.getLogger(__name__)
 

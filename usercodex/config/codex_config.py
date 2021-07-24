e045d9642df8e30536897a1a71b0ce84b5aed571
Codex51 <87869192+Codex51@users.noreply.github.com>
2021-07-24 15:56:13 +0700

    Update codex_config.py

--- usercodex/config/codex_config.py
+++ usercodex/config/codex_config.py
@@ -7,4 +7,4 @@ ENV = bool(os.environ.get("ENV", False))
 if ENV:
     from sample_config import Config  # codex
 elif os.path.exists("config.py"):
-    from config import Development as Config #codex
+    from config import Development as Config # codex

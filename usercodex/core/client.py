5081d48883bb26fd203f175955739dc1a09fc915
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-26 15:09:14 +0700

    ini ham ada 🤣

--- usercodex/core/client.py
+++ usercodex/core/client.py
@@ -38,7 +38,7 @@ sudo_enabledcmds = sudo_enabled_cmds()
 
 
 class CodexClient(TelegramClient):
-    def codex_cmd(
+    def cod_cmd(
         self: TelegramClient,
         pattern: str or tuple = None,
         info: Union[str, Dict[str, Union[str, List[str], Dict[str, str]]]]

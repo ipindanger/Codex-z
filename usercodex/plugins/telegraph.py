928b721876aee54208fb6271504a708a462528bf
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-27 01:38:54 +0700

    Fixed.

--- usercodex/plugins/telegraph.py
+++ usercodex/plugins/telegraph.py
@@ -53,3 +53,73 @@ async def _(event):
     codevent = await edit_or_reply(event, "`processing........`")
     if BOTLOG:
         await event.client.send_message(
+            BOTLOG_CHATID,
+            f"Created New Telegraph account {auth_url} for the current session. \n**Do not give this url to anyone, even if they say they are from Telegram!**",
+        )
+    optional_title = event.pattern_match.group(5)
+    if not event.reply_to_msg_id:
+        return await codevent.edit(
+            "`Reply to a message to get a permanent telegra.ph link.`",
+        )
+
+    start = datetime.now()
+    r_message = await event.get_reply_message()
+    input_str = (event.pattern_match.group(4)).strip()
+    if input_str in ["media", "m"]:
+        downloaded_file_name = await event.client.download_media(
+            r_message, Config.TEMP_DIR
+        )
+        await codevent.edit(f"`Downloaded to {downloaded_file_name}`")
+        if downloaded_file_name.endswith((".webp")):
+            resize_image(downloaded_file_name)
+        try:
+            media_urls = upload_file(downloaded_file_name)
+        except exceptions.TelegraphException as exc:
+            await codevent.edit(f"**Error : **\n`{str(exc)}`")
+            os.remove(downloaded_file_name)
+        else:
+            end = datetime.now()
+            ms = (end - start).seconds
+            os.remove(downloaded_file_name)
+            await codevent.edit(
+                f"**link : **[telegraph](https://telegra.ph{media_urls[0]})\
+                    \n**Time Taken : **`{ms} seconds.`",
+                link_preview=True,
+            )
+    elif input_str in ["text", "t"]:
+        user_object = await event.client.get_entity(r_message.sender_id)
+        title_of_page = get_display_name(user_object)
+        # apparently, all Users do not have last_name field
+        if optional_title:
+            title_of_page = optional_title
+        page_content = r_message.message
+        if r_message.media:
+            if page_content != "":
+                title_of_page = page_content
+            downloaded_file_name = await event.client.download_media(
+                r_message, Config.TEMP_DIR
+            )
+            m_list = None
+            with open(downloaded_file_name, "rb") as fd:
+                m_list = fd.readlines()
+            for m in m_list:
+                page_content += m.decode("UTF-8") + "\n"
+            os.remove(downloaded_file_name)
+        page_content = page_content.replace("\n", "<br>")
+        try:
+            response = telegraph.create_page(title_of_page, html_content=page_content)
+        except Exception as e:
+            LOGS.info(e)
+            title_of_page = "".join(
+                random.choice(list(string.ascii_lowercase + string.ascii_uppercase))
+                for _ in range(16)
+            )
+            response = telegraph.create_page(title_of_page, html_content=page_content)
+        end = datetime.now()
+        ms = (end - start).seconds
+        cod = f"https://telegra.ph/{response['path']}"
+        await codevent.edit(
+            f"**link : ** [telegraph]({cod})\
+                 \n**Time Taken : **`{ms} seconds.`",
+            link_preview=True,
+        )

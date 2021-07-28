1dcfb3d8bc97d5983b208ba4b2cb32c87b62d19d
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-07-29 02:18:03 +0700

    Fixed.

--- usercodex/plugins/quotes.py
+++ usercodex/plugins/quotes.py
@@ -7,6 +7,7 @@ from ..core.logger import logging
 from ..core.managers import edit_delete, edit_or_reply
 from ..helpers.functions import random_quote, search_quotes
 from ..helpers.utils import parse_pre
+from ..helpers import codmemes
 
 LOGS = logging.getLogger(__name__)
 plugin_category = "extra"
